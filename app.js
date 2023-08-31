260273e6c52c1e358841ebb9d4a124c10aea5d80
Muhammad Hijir Sajad <sajadhijir@gmail.com>
2023-08-31 13:12:53 +0800

    create route

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const Controller = require("./controllers");
 const app = express();
 const port = 3000;
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.get("/", Controller.home);
+app.use(require("./routes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
